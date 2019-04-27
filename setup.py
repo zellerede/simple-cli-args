6ea0e22862aa4be99a1ad853ee28fc3071b88fc0
Ber <bertalan.pecsi@nokia.com>
2019-04-27 23:43:03 +0200

    setup.py enhanced for being pippable

--- setup.py
+++ setup.py
@@ -2,8 +2,23 @@
 
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
+
 setup(
     name="simple_cli_args",
     version="0.1",
-    packages=['simple_cli_args']
+    packages=['simple_cli_args'],
+    author='Bertalan Pecsi',
+    author_email='zellerede@gmail.com',
+    description='An enhancement of argparse package for its simplest usages',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zellerede/simple-cli-args',
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
 )
